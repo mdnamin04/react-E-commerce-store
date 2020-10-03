0490bfc70186bdb7d1a3d0b3485e4a1107e3d7a1
Muhammad Amin <Amin@muhammads-mbp.home>
2020-10-03 17:03:50 -0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,117 @@
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 699.99,
+    company: "GOOGLE",
+    info:
+     "New, unlocked Android phone gives you the flexibility to change carriers and choose your own data plan; unlocked smartphone works with T-Mobile, Sprint, AT&T, Verizon, and other major carriers[1]. 5G capable[2] smartphone gives you an extra boost of speed so you can download a movie in seconds,[3] enjoy smooth streaming in ultra clear HD, play games at home and on the go,[4] and even share your 5G speed with friends[5]. Charge your cell phone wirelessly[6] or use it to wirelessly charge other Qi-certified devices[7]. The all day battery can last up to 48 hours with Extreme Battery Saver[8]. Your phone will automatically receive the latest OS and security updates for at least 3 years;[9] the custom-made Titan M chip helps secure the operating system and sensitive data, like passwords. Take vibrant photos on your phone even in the dark with Night Sight, bring studio quality light to your pictures of people with Portrait Light, and get more scenery and people in the shot with the rear facing ultrawide lens. Pixel 5 is a water resistant smart phone; the metal unibody can handle being submerged in 1.5 meters of fresh water for 30 minutes[10]",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Acer Chromebook - Spin 311",
+    img: "img/product-2.png",
+    price: 200.99,
+    company: "ACER",
+    info:
+      "Chromebook runs on Chrome OS - An operating system by Google that is built for the way we live today. It comes with built-in virus protection, updates automatically, boots up in seconds and continues to stay fast over time. (Internet connection is required).All the Google apps you know and love come standard on every Chromebook, which means you can edit, download, and convert Microsoft Office files in Google Docs, Sheets and Slides.Get access to more than 2 million Android apps from Google Play to learn and do more. Chromebooks come with built-in storage for offline access to your most important files and an additional 100GB of Google Drive space to ensure that all of your files are backed up automatically. Acer CP311-2H-C679 convertible Chromebook comes with 11.6” HD Touch IPS Display, Intel Celeron N4020, 4GB LPDDR4 Memory, 32GB eMMC, Google Chrome and Up to 10-hours battery life.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+
+    {
+    id: 3,
+    title: "Insignia NS-43DF71",
+    img: "img/product-3.png",
+    price: 219,
+    company: "INSIGNIA",
+    info:
+      "Fire TV Edition brings together live-over-the air TV and your streaming content on the home screen. Connect any HD antenna (sold separately) to watch live over-the-air TV or stream movies and shows from Disney+, Netflix, YouTube, Prime Video, Hulu, and more. Experience breathtaking 4K Ultra HD picture quality with over 8 million pixels for stunning clarity, deep contrast and vivid colors. Watch movies and TV shows come to life in ultra high deﬁnition. Everything you’d expect from a remote – plus launch apps, search for TV shows, switch inputs, control smart home devices and more, using just your voice. With Prime Video, Netflix buttons, instantly access your favorite apps. This TV is smart and simple in every way. Just plug it in, connect to Wi-Fi and enjoy. Plus, your TV keeps getting smarter with new Alexa skills and features through automatic over-the-air software updates, so that you always have the latest. Item arrives in packaging that reveals what’s inside and can’t be hidden..",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+
+    {
+    id: 4,
+    title: "Panasonic Limux GH5S",
+    img: "img/product-4.png",
+    price: 250,
+    company: "PANASONIC",
+    info:
+      "PROFESSIONAL PHOTO AND VIDEO PERFORMANCE: 10.2-megapixel Micro Four Thirds sensor and a significantly higher photoreceptive surface per pixel deliver an ultra-wide dynamic range; Dual Native ISO provides ultra-sensitive video capture with impressively low noise. RUGGED SPLASH/FREEZEPROOF DESIGN: Durable magnesium alloy body withstands heavy use out in the field and is freezeproof down to -10-degrees; Splash/dustproof construction with weather sealing on every joint, dial and button. UNLIMITED IN-CAMERA RECORDING OF C4K: Capable of internal SD card capture of 60p50p 8-bit, 30p25p24p 4:2:2 10-bit, 4K: 60p50p 4:2:0 8-bit, 30p25p24p 4:2:2 10-bit; 1080p up to 240fps and C4K 60p VFR. ANAMORPHIC VIDEO MODE: 4K Anamorphic professional video production interchangeable lens camera system enables high performance, durability and mobility. CONNECTIVITY AND PORTS: TC In/Out/Synchro Terminal (via included BNC cable), 3.5mm mic jack with line input, 3.5mm headphone jack, 2.5mm remote socket, HDMI Type A Socket and USB-C 3.1 Socket; Available twin SD Card slots (UHS-II U3 compatible)",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+
+    {
+    id: 5,
+    title: "Edifier R1700BTs Speaker",
+    img: "img/product-5.png",
+    price: 159.99,
+    company: "EDIFIER",
+    info:
+      "BLUETOOTH 5.0 WITH QUALCOMM APTX HD - Easily connect to your phone, laptop or tablet for wireless listening enjoyment, suitable for iOS, Android, Mac, or Windows devices. AptX HD supports up to 24bit quality over Bluetooth. SUB OUT - Subwoofer output with built-in crossover and auto detection. Dual RCA inputs for phone, TV, DVD, PC connection, support connect to two devices at the same time, no plugging needed. NATURAL SOUND REPRODUCTION - Get the cleanest and most impressive natural sound from the 19mm silk dome tweeter unit, 4 inches woofer unit, and 66W continuous undistorted power. UPDATED WIRELESS REMOTE - Easily control volume, mute/unmute, Switch between Bluetooth and line-in modes. Adjust the bass, treble, volume on the side panel. Enhance your listing experience with Soundfield Spatializer button. 2 YEAR WARRANTY - Guaranteed high quality and reliability with hassle-free parts and labor warranty for two years in USA and Canada.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+
+    {
+    id: 6,
+    title: "Bose Bluetooth Headphones 700",
+    img: "img/product-6.png",
+    price: 379.00,
+    company: "BOSE",
+    info:
+      "Keep your phone in your pocket and your head up to the world with easy access to voice assistants for music, navigation, weather, and more. Turn any space into a workplace — with powerful noise cancelling, astonishing sound, unrivaled voice pickup. Personalize your environment with 11 levels of noise cancelling control distractions or let ambient sound in. Access Alexa with the push of a button, or use the wake up word. Optimized for the Google Assistant; Access your voice assistant with a simple button press. Listen comfortably for hours a streamlined, lightweight stainless steel headband and angled ear cups make for a perfect fit. Get up to 20 hours of wireless battery life and time based power information",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+
+  {
+    id: 7,
+    title: "iRobot Roomba 614",
+    img: "img/product-7.png",
+    price: 141.50,
+    company: "IRobot",
+    info:
+      "3-Stage cleaning system loosens, lifts, and suctions dirt, dust, and hair from hard floors and carpet. Dual Multi Surface Brushes work together to grab dust, dirt, and large debris; The Auto Adjust cleaning head automatically adapts its height to effectively clean both carpets and hard floors. Patented Dirt Detect sensors alert Roomba to work harder on concentrated areas of dirt, such as high traffic zones of your home. Full suite of sensors intelligently navigate the robot under and around objects and under furniture to help thoroughly clean your floors. Edge Sweeping Brush is specially designed at a 27 degree angle to sweep debris away from edges and corners. Runs for upto 90 minutes before automatically docking and recharging. Featuring Roomba Essentials Smart Navigation, Auto Adjust Cleaning Head, Works on Carpets and Hard Floors",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+
+  {
+    id: 8,
+    title: "Canon PIXMA TR4520",
+    img: "img/product-8.png",
+    price: 114.45,
+    company: "CANON",
+    info:
+      "With Wireless Connect and the Canon Print app, use your smartphone to quickly and easily set up your printer and print right from your smartphone or tablet. With features like AirPrint, Mopria Print Service, Auto Power On/Off, a built in ADF, and Wi Fi it's easy to see why PIXMA printers are so simple to use. With IFTTT (IF This Then That) support, use various web services and applications to automate printing, and save time and hassle. Connectivity Technology: USB. Printer Technology: Inkjet. Save 10% on all ink reorders with Dash Replenishment; Upon activation your printer measures ink levels and places smart reorders when you are running low; No subscription fees; Cancel anytime",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+]
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 699.99,
+  company: "google",
+  info:
+     "New, unlocked Android phone gives you the flexibility to change carriers and choose your own data plan; unlocked smartphone works with T-Mobile, Sprint, AT&T, Verizon, and other major carriers[1]. 5G capable[2] smartphone gives you an extra boost of speed so you can download a movie in seconds,[3] enjoy smooth streaming in ultra clear HD, play games at home and on the go,[4] and even share your 5G speed with friends[5]. Charge your cell phone wirelessly[6] or use it to wirelessly charge other Qi-certified devices[7]. The all day battery can last up to 48 hours with Extreme Battery Saver[8]. Your phone will automatically receive the latest OS and security updates for at least 3 years;[9] the custom-made Titan M chip helps secure the operating system and sensitive data, like passwords. Take vibrant photos on your phone even in the dark with Night Sight, bring studio quality light to your pictures of people with Portrait Light, and get more scenery and people in the shot with the rear facing ultrawide lens. Pixel 5 is a water resistant smart phone; the metal unibody can handle being submerged in 1.5 meters of fresh water for 30 minutes[10]",
+  inCart: false,
+  count: 0,
+  total: 0
+};
